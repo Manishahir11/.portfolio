45bc19a06b0ae4b42190ecb908b3276520d73bc0
manish <manishahir41@gmail.com>
2023-09-14 00:43:52 +0530

    box-shado add

--- Typed.js
+++ Typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#element', {
-    strings: ['web devlopar', 'Frontend Developer ','Backend Developer '],
+    strings: ['Web Developar', 'Frontend Developer ','Backend Developer '],
     typeSpeed: 100,
     backspeed: 100,
     backDelay:1000,
