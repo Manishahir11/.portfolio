053f645c2a6224d87ae0189aa8fa22c18a1ea64d
manish <manishahir41@gmail.com>
2023-09-13 23:01:31 +0530

    typed javascript add

---
+++ Typed.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+    strings: ['web devlopar', 'Frontend Developer ','Backend Developer '],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay:1000,
+    loop:true
+  });
+  
+  
