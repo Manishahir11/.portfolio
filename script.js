0adbb12b140029d1f959bbd8d2eb75c6eb1f9443
manish <manishahir41@gmail.com>
2023-09-27 22:09:14 +0530

    update 27-09-23

---
+++ script.js
@@ -0,0 +1,15 @@
+// function absection(){
+//     alert("yes ")
+// console.log("working");
+// console.log("working");
+// document.querySelector(".about").addEventListener("click",(e) => {
+// //     document.querySelector(".about").innerHTML = 
+// })
+// }
+// function about(params) {
+//     alert("yes ")
+//     document.querySelector(".about")
+// // .addEventListener("click",() => {
+// //    document.querySelector(".about").innerHTML})
+    
+// }
